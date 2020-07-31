6e4d1c34b93c1f4152f1f8303701035f0a8ade85
10098 <chanhee.won@sk.com>
2020-07-31 15:26:00 +0900

    1st commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('Hello!!!!! Hello!!!!! Hello!!!!!');
+    res.send('Hello!!!!! Hello!!!!!');
 });
 
 app.get('/start', function(req, res){
